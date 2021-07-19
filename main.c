883bbaa09bbf3b3ee681a76bc88f650b5344e729
AndreyTheHacker <andrejpavlenko666@gmail.com>
2021-07-19 16:13:02 +0900

    Added description

--- main.c
+++ main.c
@@ -3,6 +3,9 @@ __asm__("jmpl $0x0000, $main\n");
 
 void kern_putc(const int c) {
 	__asm__ __volatile__("int $0x10"::"a"(0x0e00|c),"b"(0x0007));
+	//										^ ^			  ^ ^
+	//										|  \		  |  \
+	//										al ah         bl bh
 }
 
 void kern_printc(const char *c) {
@@ -15,5 +18,10 @@ void kern_printc(const char *c) {
 
 void main() {
 	kern_printc("Hello! Welcome!");
-	while(1){}
+	while(1){
+		/*
+			Doing nothing, because if I call kern_putc('A'),
+			Output will be: A AA[smile]AAAAAAAAA
+		*/
+	}
 }
