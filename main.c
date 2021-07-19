0b6eab0e588459024092c7b0e26d2681cf4ee649
AndreyTheHacker <andrejpavlenko666@gmail.com>
2021-07-19 16:24:18 +0900

    Updated description

--- main.c
+++ main.c
@@ -3,9 +3,17 @@ __asm__("jmpl $0x0000, $main\n");
 
 void kern_putc(const int c) {
 	__asm__ __volatile__("int $0x10"::"a"(0x0e00|c),"b"(0x0007));
-	//										^ ^			  ^ ^
-	//										|  \		  |  \
-	//										al ah         bl bh
+	// "a"(0x0e00|c)
+	// |- a is ax register
+	// |- 0x0e00 is ah (0x0e) and al(0x00) register (little-endian)
+	// |- b is bx register
+	// |- 0x0007 is bh (0x00) and bl(0x07) register (little-endian)
+
+	// ==int 0x10==
+	// ah is function
+	// al is char to print
+	// bh is video page
+	// bl is color
 }
 
 void kern_printc(const char *c) {
